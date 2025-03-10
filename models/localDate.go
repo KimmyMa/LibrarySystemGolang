92a6a896cd23add31504ce6b6e15af603d683e5b
KimmaMa <k1mmyma@qq.com>
2025-03-10 14:26:51 +0800

    update password check and lend/return book confirm

--- models/localDate.go
+++ models/localDate.go
@@ -22,7 +22,6 @@ func (t *LocalDate) UnmarshalJSON(data []byte) (err error) {
 }
 func (t LocalDate) MarshalJSON() ([]byte, error) {
 	tt := time.Time(t)
-	fmt.Println(tt)
 	if tt.IsZero() {
 		return []byte(`""`), nil
 	}
@@ -33,39 +32,19 @@ func (t LocalDate) MarshalJSON() ([]byte, error) {
 	return b, nil
 }
 
-//func (t LocalDate) MarshalJSON() ([]byte, error) {
-//	b := make([]byte, 0, len(TimeFormat)+2)
-//	b = append(b, '"')
-//	b = time.Time(t).AppendFormat(b, TimeFormat)
-//	b = append(b, '"')
-//	return b, nil
-//}
-
 func (t LocalDate) Value() (driver.Value, error) {
 	if t.String() == "0001-01-01" {
 		return nil, nil
 	}
+	if t.IsZero() {
+		return nil, nil // 返回 nil 表示数据库中的 NULL
+	}
 	return []byte(time.Time(t).Format(TimeFormat)), nil
 }
 
-//	func (t *LocalDate) Scan(v interface{}) error {
-//		if v == nil {
-//			*t = LocalDate(time.Time{})
-//			fmt.Println("v is nil, t is ", t)
-//			return nil
-//		}
-//		tTime, err := time.Parse("2006-01-02 15:04:05 +0800 CST", v.(time.Time).String())
-//		if err != nil {
-//			return err
-//		}
-//		*t = LocalDate(tTime)
-//		fmt.Println(t)
-//		return nil
-//	}
 func (t *LocalDate) Scan(v interface{}) error {
 	if v == nil {
 		*t = LocalDate(time.Time{})
-		//fmt.Println("Scanned NULL value")
 		return nil
 	}
 
@@ -73,21 +52,24 @@ func (t *LocalDate) Scan(v interface{}) error {
 	switch v := v.(type) {
 	case time.Time:
 		*t = LocalDate(v)
-		//fmt.Println("Scanned time.Time value:", v)
 	case string:
 		tTime, err := time.Parse(TimeFormat, v)
 		if err != nil {
 			return err
 		}
 		*t = LocalDate(tTime)
-		//fmt.Println("Scanned string value:", v)
 	default:
 		return fmt.Errorf("unsupported type: %T", v)
 	}
 	return nil
 }
+
+func (t LocalDate) IsZero() bool {
+	return time.Time(t).IsZero()
+}
+
 func (t LocalDate) String() string {
-	if time.Time(t).Year() == 1 {
+	if t.IsZero() {
 		return ""
 	}
 	return time.Time(t).Format(TimeFormat)
